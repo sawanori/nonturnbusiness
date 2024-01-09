15cd65c2afd32435e11617c6d2bdd79888d14e8d
noritakasawada <snp.inc.info@gmail.com>
2024-01-09 23:12:16 +0900

    up06

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const nextConfig = {}
 module.exports = {
   nextConfig, 
   images: {
-   domains: ['flowbite.s3.amazonaws.com'],  
+   domains: ['flowbite.s3.amazonaws.com', 'rpk6snz1bj3dcdnk.public.blob.vercel-storage.com'],  
   }
 }
