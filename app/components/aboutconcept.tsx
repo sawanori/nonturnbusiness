ce372f7ad769fcb596f15206d1b6a00d9b84b9c7
noritakasawada <snp.inc.info@gmail.com>
2024-01-10 23:16:09 +0900

    up15

--- app/components/aboutconcept.tsx
+++ app/components/aboutconcept.tsx
@@ -17,8 +17,8 @@ export const AboutConcept = () => {
                     renderContent={({ progress }) =>
                         <WorkContainer>
                             <WorkLeft progress={progress}>
-                                <h1 className=' font-extrabold text-5xl md:text-3xl lg:text-4xl xl:text-5xl  my-3    text-transparent bg-clip-text bg-gradient-to-r from-cyan-50 to-stone-700'>中小企業の悩み</h1>
-                                <p className='text-xl md:text-xl lg:text-2xl  xl:text-3xl tracking-tight'>ホームページやSNSを使って<br />自社の認知を広めたいが<br />自信を持って表に出せる<br/>宣伝素材がなく<br/>結果、手付かずになる</p>
+                                <h1 className=' font-extrabold text-4xl md:text-2xl lg:text-4xl xl:text-5xl  my-3    text-transparent bg-clip-text bg-gradient-to-r from-cyan-50 to-stone-700'>中小企業の悩み</h1>
+                                <p className='text-xl md:text-xl lg:text-1xl  xl:text-2xl tracking-tight'>WEBで自社の認知を広めたいが<br />自信を持って表に出せる<br/>宣伝素材がなく<br/>結果、手付かずになる</p>
                             </WorkLeft>
                             <WorkRight progress={progress}>
                                 <Image src="/verticaal.jpg" alt="説明文" layout='responsive' width={100} height={100} />
@@ -31,8 +31,8 @@ export const AboutConcept = () => {
                     renderContent={({ progress }) =>
                         <WorkContainer>
                             <WorkLeft progress={progress}>
-                                <h1 className='font-extrabold text-5xl md:text-3xl lg:text-4xl xl:text-5xl  my-3     text-transparent bg-clip-text bg-gradient-to-r from-cyan-50 to-stone-700'>我々の解決できる事</h1>
-                                <p className='text-xl md:text-xl lg:text-2xl  xl:text-3xl tracking-tight'>貴社の目的をヒアリングした上で<br />最適な表現方法を選定<br />スピーディーにご提供</p>
+                                <h1 className='font-extrabold text-3xl md:text-2xl lg:text-3xl xl:text-4xl  my-3     text-transparent bg-clip-text bg-gradient-to-r from-cyan-50 to-stone-700'>我々の解決できる事</h1>
+                                <p className='text-xl md:text-xl lg:text-1xl  xl:text-2xl tracking-tight'>貴社の目的をヒアリングした上で<br />最適な表現方法を選定<br />スピーディーにご提供</p>
 
                             </WorkLeft>
                             <WorkRight progress={progress}>
@@ -47,8 +47,8 @@ export const AboutConcept = () => {
                     renderContent={({ progress }) =>
                         <WorkContainer>
                             <WorkLeft progress={progress}>
-                                <h1 className='font-extrabold text-5xl md:text-3xl lg:text-4xl xl:text-5xl  my-3     text-transparent bg-clip-text bg-gradient-to-r from-cyan-50 to-stone-700'>共に目指す場所</h1>
-                                <p className='text-xl md:text-xl lg:text-2xl  xl:text-3xl tracking-tight'>企業イメージのアップから<br />事業の告知や実際の集客<br />皆様のビジネスの結果に繋がるクリエイティブを目指します</p>
+                                <h1 className='font-extrabold text-3xl md:text-2xl lg:text-3xl xl:text-5xl  my-3     text-transparent bg-clip-text bg-gradient-to-r from-cyan-50 to-stone-700'>共に目指す場所</h1>
+                                <p className='text-xl md:text-xl lg:text-2xl  xl:text-3xl tracking-tight'>企業イメージのアップから<br />事業の告知や実際の集客<br />結果に向かって二人三脚</p>
 
                             </WorkLeft>
                             <WorkRight progress={progress}>
